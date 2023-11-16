652edd1f1286d7e72a1ce7f7fe10b7e079315bd0
Nicolas Delille <delille.n@gmail.com>
2023-11-16 12:40:23 +0100

    Optional

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,7 @@
+require 'faker'
+require 'uri'
+require 'json'
+
 # This file should ensure the existence of records required to run the application in every environment (production,
 # development, test). The code here should be idempotent so that it can be executed at any point in every environment.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
@@ -7,3 +11,27 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+Movie.destroy_all
+
+Movie.create(title: "Wonder Woman 1984", overview: "Wonder Woman comes into conflict with the Soviet Union during the Cold War in the 1980s", poster_url: "https://image.tmdb.org/t/p/original/8UlWHLMpgZm9bx6QYh0NFoq67TZ.jpg", rating: 6.9)
+Movie.create(title: "The Shawshank Redemption", overview: "Framed in the 1940s for double murder, upstanding banker Andy Dufresne begins a new life at the Shawshank prison", poster_url: "https://image.tmdb.org/t/p/original/q6y0Go1tsGEsmtFryDOJo3dEmqu.jpg", rating: 8.7)
+Movie.create(title: "Titanic", overview: "101-year-old Rose DeWitt Bukater tells the story of her life aboard the Titanic.", poster_url: "https://image.tmdb.org/t/p/original/9xjZS2rlVxm8SFx8kPC3aIGCOYQ.jpg", rating: 7.9)
+Movie.create(title: "Ocean's Eight", overview: "Debbie Ocean, a criminal mastermind, gathers a crew of female thieves to pull off the heist of the century.", poster_url: "https://image.tmdb.org/t/p/original/MvYpKlpFukTivnlBhizGbkAe3v.jpg", rating: 7.0)
+
+50.times do
+  Movie.create(title: Faker::Movie.title, overview: Faker::Quote.famous_last_words, poster_url: Faker::LoremFlickr.image, rating: Faker::Number.decimal(l_digits: 2))
+end
+
+
+
+# url = "http://tmdb.lewagon.com/movie/top_rated?language=en-US&page=1"
+# data_serialized = URI.open(url).read
+# data = JSON.parse(data_serialized)
+
+# results = data["results"]
+
+# results.each do |result|
+#   puts "#{result["original_title"]}"
+#   Movie.create(title: "", overview: "", poster_url: ".jpg", rating: 6.9)
+# end
